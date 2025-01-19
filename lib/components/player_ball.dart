7af05c4b1a8479e4a059d5d2f0d7598fe20ef2da
丁少华 <960423114@qq.com>
2025-01-19 15:27:36 +0800

    fix

--- lib/components/player_ball.dart
+++ lib/components/player_ball.dart
@@ -24,8 +24,15 @@ class PlayerBall extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final playerState = ref.watch(playerStateProvider);
+    // 获取当前设备的MediaQuery数据
+    var mediaQueryData = MediaQuery.of(context);
+    // 判断是否是Pad
+    bool isPad = mediaQueryData.size.width > 700;
+    print('mediaQueryData.size.width');
+    print(mediaQueryData.size.width);
+    print(mediaQueryData.size.width > 700);
 
+    final playerState = ref.watch(playerStateProvider);
     // if (playerState.status == 'playing') {
     //   player.play(playerState.song.url);
     // }
@@ -38,78 +45,87 @@ class PlayerBall extends ConsumerWidget {
         left: 0,
         bottom: 0, // 控制浮动按钮的位置，确保它不会被BottomNavigationBar遮挡
         child: Center(
-            child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
-                width: 260,
-                height: 40,
-                // color: Colors.red,
-                decoration: BoxDecoration(
-                  border:
-                  Border.all(width: 0, color: Colors.deepOrangeAccent),
-                  color: Colors
-                      .deepOrangeAccent, // Background color of the container
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30), // 左上角圆角
-                    topRight: Radius.circular(30), // 右上角圆角
-                    bottomLeft: Radius.circular(0), // 左下角圆角
-                    bottomRight: Radius.circular(0), // 右下角圆角
-                  ), // Round all corners with a radius of 16
-                ),
-                child: Row(children: [
-                  Row(
-                    children: [
-                      playerState.coverUrl.isEmpty
-                          ? SvgPicture.asset('assets/img/music.svg',
-                              width: 30, height: 30)
-                          : Image.network(playerState.coverUrl,
-                              width: 30, height: 30),
-                      Container(
-                        margin: const EdgeInsets.only(left: 18), // 设置外边距
-                        width: 80, // 设置宽度
-                        height: 30,
-
-                        child: Marquee(
-                          text: playerState.songName,
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.white),
-                          blankSpace: 20, // 跑马灯前后留白
-                          velocity: 10, // 滚动速度
-                          pauseAfterRound: const Duration(seconds: 2), // 滚动停止时间
+            child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 700), // 设置最大宽度为300
+                child: FractionallySizedBox(
+                    widthFactor: isPad ? 0.5 : 0.68,
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 6),
+                        width: double.infinity,
+                        height: 40,
+                        // color: Colors.red,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 0, color: Colors.deepOrangeAccent),
+                          color: Colors
+                              .deepOrangeAccent, // Background color of the container
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(30), // 左上角圆角
+                            topRight: Radius.circular(30), // 右上角圆角
+                            bottomLeft: Radius.circular(0), // 左下角圆角
+                            bottomRight: Radius.circular(0), // 右下角圆角
+                          ), // Round all corners with a radius of 16
                         ),
-                      )
-                    ],
-                  ),
-                  Container(
-                      margin: const EdgeInsets.only(left: 10),
-                      child: Row(
-                        children: [
-                          SvgPicture.asset("assets/img/player-skip-back.svg",
-                              semanticsLabel: 'Dart Logo',
-                              width: 25,
-                              height: 25,
-                              colorFilter: const ColorFilter.mode(
-                                  Colors.white, BlendMode.srcIn)),
-                          InkWell(
-                              onTap: () {
-                                onTapPlay(ref);
-                              },
-                              child: SvgPicture.asset(
-                                playIcon,
-                                semanticsLabel: 'Dart Logo',
-                                width: 25,
-                                height: 25,
-                                colorFilter: const ColorFilter.mode(
-                                    Colors.white, BlendMode.srcIn),
-                              )),
-                          SvgPicture.asset("assets/img/player-skip-forward.svg",
-                              semanticsLabel: 'Dart Logo',
-                              width: 25,
-                              height: 25,
-                              colorFilter: const ColorFilter.mode(
-                                  Colors.white, BlendMode.srcIn)),
-                        ],
-                      ))
-                ]))));
+                        child: Row(children: [
+                          playerState.coverUrl.isEmpty
+                              ? SvgPicture.asset('assets/img/music.svg',
+                                  width: 30, height: 30)
+                              : Image.network(playerState.coverUrl,
+                                  width: 30, height: 30),
+                          // 按比例分配剩余空间的元素
+                          Flexible(
+                            flex: 1, // 占剩余空间的1份
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 18), // 设置外边距
+                              height: 30,
+                              child: Marquee(
+                                text: playerState.songName,
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                                blankSpace: 20, // 跑马灯前后留白
+                                velocity: 10, // 滚动速度
+                                pauseAfterRound:
+                                    const Duration(seconds: 2), // 滚动停止时间
+                              ),
+                            ),
+                          ),
+
+                          Container(
+                              margin: const EdgeInsets.only(left: 10),
+                              width: 120,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment
+                                    .spaceBetween, // 使用 space-between 效果
+                                children: [
+                                  SvgPicture.asset(
+                                      "assets/img/player-skip-back.svg",
+                                      semanticsLabel: 'Dart Logo',
+                                      width: 23,
+                                      height: 23,
+                                      colorFilter: const ColorFilter.mode(
+                                          Colors.white, BlendMode.srcIn)),
+                                  InkWell(
+                                      onTap: () {
+                                        onTapPlay(ref);
+                                      },
+                                      child: SvgPicture.asset(
+                                        playIcon,
+                                        semanticsLabel: 'Dart Logo',
+                                        width: 23,
+                                        height: 23,
+                                        colorFilter: const ColorFilter.mode(
+                                            Colors.white, BlendMode.srcIn),
+                                      )),
+                                  SvgPicture.asset(
+                                      "assets/img/player-skip-forward.svg",
+                                      semanticsLabel: 'Dart Logo',
+                                      width: 23,
+                                      height: 23,
+                                      colorFilter: const ColorFilter.mode(
+                                          Colors.white, BlendMode.srcIn)),
+                                ],
+                              ))
+                        ]))))));
   }
 }
--- lib/components/scaffold_bar.dart
+++ lib/components/scaffold_bar.dart
@@ -3,7 +3,6 @@ import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:imusic/components/player_ball.dart';
 
-
 /*
 * 定义标签栏和标签页
 * */
@@ -22,33 +21,110 @@ var _barItems = <BottomNavigationBarItem>[
       label: '我的'),
 ];
 
-// 定义ScaffoldWithNavBar （底部导航和页面展示框架）
-class ScaffoldWithNavBar extends StatelessWidget {
-  const ScaffoldWithNavBar({
-    required this.navigationShell,
-    this.showNavBar=false,
-    Key? key,
-  }) : super(key: key ?? const ValueKey('ScaffoldWithNavBar'));
-
+class ScaffoldWithNavBar extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
   final bool showNavBar;
 
-  @override
-  Widget build(context) {
+  const ScaffoldWithNavBar({
+    required this.navigationShell,
+    this.showNavBar = false,
+    Key? key,
+  }) : super(key: key ?? const ValueKey('ScaffoldWithNavBar'));
 
-    // final location = GoRouter.of(context).location;
+  @override
+  _ScaffoldWithNavBarState createState() => _ScaffoldWithNavBarState();
+}
+
+class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 初始化动画控制器
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    // 定义高度动画
+    _heightAnimation = Tween<double>(  // 用于定义动画的起始和结束值
+      // 表示动画开始时的高度为 0 像素（假设这是底部导航栏的高度），end: 60 表示动画结束时的高度为 60 像素（完全隐藏）。
+      begin: 0,
+      end: 56,
+    ).animate(CurvedAnimation(
+      parent: _controller, // parent 参数绑定到 _controller
+      curve: Curves.easeInOut, // curve 参数设置为 Curves.easeInOut，表示动画在开始和结束时都会逐渐加速和减速，中间部分速度较快。
+    ));
+
+
+
+    // 初始化动画状态
+    if (widget.showNavBar) {
+      print("显示");
+      _controller.forward(); // 显示
+    } else {
+      print("隐藏");
+      _controller.reverse(); // 隐藏
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant ScaffoldWithNavBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    // 根据 showNavBar 的变化触发动画
+    if (widget.showNavBar) {
+      print("显示");
+      _controller.forward(); // 显示
+    } else {
+      print("隐藏");
+      _controller.reverse(); // 隐藏
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Stack(children: [navigationShell, const PlayerBall()]),
-      bottomNavigationBar: showNavBar?BottomNavigationBar(
-        items: _barItems,
-        currentIndex: navigationShell.currentIndex,
-        onTap: (int index) => _onTap(context, index),
-      ):null,
+      body: Stack(
+        children: [
+          widget.navigationShell,
+          const PlayerBall(), // 悬浮播放小球
+        ],
+      ),
+      bottomNavigationBar: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          // 当高度接近 0 时，移除导航栏内容
+          if (_heightAnimation.value <= 1) {
+            return const SizedBox.shrink(); // 返回空组件以释放空间
+          }
+          return SizedBox(
+            height: _heightAnimation.value, // 动态调整高度
+            child: child,
+          );
+        },
+        child: BottomNavigationBar(
+          items: _barItems,
+          currentIndex: widget.navigationShell.currentIndex,
+          onTap: (int index) => _onTap(context, index),
+        ),
+      ),
     );
   }
 
-  void _onTap(context, index) {
-    var initialLocation = index == navigationShell.currentIndex;
-    navigationShell.goBranch(index, initialLocation: initialLocation);
+  void _onTap(BuildContext context, int index) {
+    var initialLocation = index == widget.navigationShell.currentIndex;
+    widget.navigationShell.goBranch(index, initialLocation: initialLocation);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
